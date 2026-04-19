8cd54045bbafa95b5f114323880fdb3eaab650c6
Roman Berger <romanberger001@gmail.com>
2026-04-19 13:55:37 +0200

    Interatctive token generation
    Basic interactive token generation with Read-Eval-Print-Loop.
    No real Eval yet, but tokens are generated from the user input.

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"interpreter/repl"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s, this is the Mokey Programming Language!\n", user.Username)
+	fmt.Printf("Feel free to type a command\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
